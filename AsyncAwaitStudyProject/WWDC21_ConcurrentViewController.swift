3a8700e40e2f5072f0311f7c3ec1fa1cbc4a18f0
Hyunndy <yoosa3004@naver.com>
2023-02-16 01:02:03 +0900

    WWDC Structured Concurrency 끝 !! 끝 끝 아나

--- AsyncAwaitStudyProject/WWDC21_ConcurrentViewController.swift
+++ AsyncAwaitStudyProject/WWDC21_ConcurrentViewController.swift
@@ -200,3 +200,173 @@ class ViewController2: UIViewController {
         return thumbnails
     }
 }
+
+@MainActor
+class MyDelegate: UICollectionViewDelegate {
+    func isEqual(_ object: Any?) -> Bool {
+        <#code#>
+    }
+    
+    var hash: Int
+    
+    var superclass: AnyClass?
+    
+    func `self`() -> Self {
+        <#code#>
+    }
+    
+    func perform(_ aSelector: Selector!) -> Unmanaged<AnyObject>! {
+        <#code#>
+    }
+    
+    func perform(_ aSelector: Selector!, with object: Any!) -> Unmanaged<AnyObject>! {
+        <#code#>
+    }
+    
+    func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!) -> Unmanaged<AnyObject>! {
+        <#code#>
+    }
+    
+    func isProxy() -> Bool {
+        <#code#>
+    }
+    
+    func isKind(of aClass: AnyClass) -> Bool {
+        <#code#>
+    }
+    
+    func isMember(of aClass: AnyClass) -> Bool {
+        <#code#>
+    }
+    
+    func conforms(to aProtocol: Protocol) -> Bool {
+        <#code#>
+    }
+    
+    func responds(to aSelector: Selector!) -> Bool {
+        <#code#>
+    }
+    
+    var description: String
+    
+    
+    /*
+     UI 작업은 메인스레드에서 발생해야 하며
+     Swift Actor 세션에도 있는것처럼
+     Swift는 @MainActor라고 표기함으로써 이걸 보장합니다.
+     
+     컬렉션뷰가 있고,
+     우리는 Cell에 네트워크에서 썸네일을 가져와 표기하려고합니다.
+     하지만! Delegate는 async하지 않으므로 async 함수를 호출할 수 없씁니다. await할 수 없기 떄문이죠 ㅠㅠㅠㅠ
+     
+     당장 Task를 시작해야지만,
+     Task는 delegate action에 대한 응답으로 시작한 work의 확장입니다(??)
+     우리는 이 new Task가 UI 우선순위로 MainActor에서 계속 실행되기를 원합니다.
+     
+     우리는 이 Task의 수명을 이 싱글 delegate 함수에 바인딩하고싶지 않습니다!
+     
+     이러한 상황에서..!!
+     우리는 Unstrucutured task를 만들 수 있습니다.
+     */
+//    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt item: IndexPath) {
+//        let ids = getThumbnailIds(for: item)
+//        let thumbnails = await fetchThumbnail2(for: ids)
+//    }
+
+    /*
+     코드의 비동기 부분을 클로저로 이동하고,
+     해당 클로저를 전달하며 비동기 작업을 구성해보겠습니다.
+     
+     이렇게 하면 무슨일이 일어나냐면
+     Task를 생성하는 시점에 도달하면 Swift는 original scope와 동일한 Actor에서 실행되도록 스케줄링 합니다. (Task는 자동 스케줄링됨)
+     이 경우에는 Main Actor 입니다!
+     그사이에 control은 호출자에게 즉시 반환됩니다. 함수 말하는거겠죠?
+     
+     썸네일 받아오는 Task는 delegate 함수에서 Main Thread를 차단하지 않고 기회가 있을 때 Main Thread에서 실행됩니다.
+     이런 식으로 Task를 생성하면 구조화된 코드와 구조화되지 않은 코드 사이의 중간지점을 제공합니다.
+    
+     */
+//    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt item: IndexPath) {
+//        let ids = getThumbnailIds(for: item)
+//        Task {
+//            let thumbnails = try await fetchThumbnail2(for: ids)
+//            display(thumbnails, in: cell)
+//        }
+//    }
+    /*
+     위 코드는 unscope Task로 만약에 썸네일이 로드되기전에 스크롤되서 다시 로드해야하면 이 Task는 명시적으로 취소해줘야한다.
+     */
+    
+    var thumbnailTasks: [IndexPath: Task<Void, Error>] = [:]
+//
+//    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt item: IndexPath) {
+//        let ids = getThumbnailIds(for: item)
+//        thumbnailTasks[item] = Task {
+//            defer { thumbnailTasks[item] = nil } // defer가 뭐징
+//            let thumbnails = try await fetchThumbnail2(for: ids)
+//            display(thumbnails, in: cell)
+//        }
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+//        thumbnailTasks[indexPath]?.cancel()
+//    }
+//
+  
+    /*
+     서버에서 썸네일을 가져온 후 나중에 가져오려고 할 때 네트워크에 다시 연결되지 않도록 로컬 디스크 캐시에 기록하려고 한다고 가정해보자.
+     캐싱은 메인 액터에서 발생할 필요가 없으며 모든 썸네일 가져오기를 취소하더라도 가져온 썸네일을 캐시하는것이 여전히 유용합니다.
+     */
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt item: IndexPath) {
+        let ids = getThumbnailIds(for: item)
+        thumbnailTasks[item] = Task {
+            defer { thumbnailTasks[item] = nil } // defer가 뭐징
+            let thumbnails = try await fetchThumbnail2(for: ids)
+            display(thumbnails, in: cell)
+            
+            /*
+             Datached Task
+             로컬 디스크에 캐싱하는거니까 낮은 우선순위의 백그라운드에 지정합니다.
+             */
+            Task.detached(.background, operation: {
+                writeToLocalcache(thumbnails)
+            })
+        }
+    }
+    
+    
+    
+    func display(_ dic: [String: UIImage], in: UICollectionViewCell) {
+        
+    }
+    
+    /// Asynchronous code with async/await is structured
+    /// JUST Async/Await
+    func fetchThumbnail2(for ids: [String]) async throws -> [String: UIImage] {
+        var thumbnails: [String: UIImage] = [:]
+        
+        for id in ids {
+            let request = URLRequest(url: URL(string: id)!)
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            try validateResponse(response)
+            guard let image = await UIImage(data: data)?.byPreparingThumbnail(ofSize: CGSize(width: 40.0, height: 40.0)) else {
+                throw HyunndyError.noImage
+            }
+            
+            thumbnails[id] = image
+        }
+        
+        return thumbnails
+    }
+    
+    func getThumbnailIds(for: IndexPath) -> [String] {
+        return ["아"]
+    }
+}
+
+
+class testViewController2: UIViewController {
+    
+    
+}
